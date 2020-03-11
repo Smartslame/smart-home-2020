52d5f245619b841021ea8a357313690278170996
Aleksandr Iakushkin <iakushkin@yandex-team.ru>
2020-03-11 22:35:15 +0300

    Change Door constructor signature

--- src/main/java/ru/sbt/mipt/oop/component/Door.java
+++ src/main/java/ru/sbt/mipt/oop/component/Door.java
@@ -9,7 +9,7 @@ public class Door implements Actionable {
     private final String id;
     private boolean isOpen;
 
-    public Door(boolean isOpen, String id) {
+    public Door(String id, boolean isOpen) {
         this.isOpen = isOpen;
         this.id = id;
     }
