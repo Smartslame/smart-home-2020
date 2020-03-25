2cf4bd07eafb47d0de6377e8f9e0929e2940498f
Alexander Yakushkin <smartslame@yandex.ru>
2020-03-25 12:48:24 +1000

    Add UNSUPPORTED_EVENT sensor event type

--- src/main/java/ru/sbt/mipt/oop/adapter/CCSensorEventAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/adapter/CCSensorEventAdapter.java
@@ -9,13 +9,13 @@ import java.util.Map;
 
 public class CCSensorEventAdapter extends SensorEvent {
     private static final Map<String, SensorEventType> ccEventTypeToEventType = new HashMap<String, SensorEventType>() {{
-            put("LightIsOn", SensorEventType.LIGHT_ON);
-            put("LightIsOff", SensorEventType.LIGHT_OFF);
-            put("DoorIsOpen", SensorEventType.DOOR_OPEN);
-            put("DoorIsClosed", SensorEventType.DOOR_CLOSED);
+        put("LightIsOn", SensorEventType.LIGHT_ON);
+        put("LightIsOff", SensorEventType.LIGHT_OFF);
+        put("DoorIsOpen", SensorEventType.DOOR_OPEN);
+        put("DoorIsClosed", SensorEventType.DOOR_CLOSED);
     }};
 
     public CCSensorEventAdapter(CCSensorEvent event) {
-        super(ccEventTypeToEventType.get(event.getEventType()), event.getObjectId());
+        super(ccEventTypeToEventType.getOrDefault(event.getEventType(), SensorEventType.UNSUPPORTED_EVENT), event.getObjectId());
     }
 }
